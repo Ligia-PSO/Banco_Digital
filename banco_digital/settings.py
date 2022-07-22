d4371c632c7ed92896c2799f12a2f59b2fbef493
Enderson Menezes <endersonster@gmail.com>
2022-07-22 12:13:08 -0300

    Revert "Update:mudança para base de dados sqlite3"
    This reverts commit f4376c54a2cca5d3b0eec1bace527c0cabf6d907.

--- banco_digital/settings.py
+++ banco_digital/settings.py
@@ -75,26 +75,20 @@ TEMPLATES = [
 WSGI_APPLICATION = 'banco_digital.wsgi.application'
 
 
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.{}'.format(os.getenv('DATABASETYPE')),
+        'NAME': os.getenv('NAME'), 
+        'USER': os.getenv('USER'), 
+        'PASSWORD': os.getenv('PASSWORD'), 
+        'HOST': os.getenv('HOST'), 
+        'PORT': os.getenv('PORT'), 
     }
 }
-
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.{}'.format(os.getenv('DATABASETYPE')),
-#         'NAME': os.getenv('NAME'), 
-#         'USER': os.getenv('USER'), 
-#         'PASSWORD': os.getenv('PASSWORD'), 
-#         'HOST': os.getenv('HOST'), 
-#         'PORT': os.getenv('PORT'), 
-#     }
-# }
 
 
 
